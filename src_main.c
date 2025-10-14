cf9c7aac665bc0379f643c87224cc20f7828ca9c
Júlia Almeida <julia.almeidabm@gmail.com>
2025-10-14 20:51:44 -0300

    Add files via upload

---
+++ src_main.c
@@ -0,0 +1,15 @@
+ 
+ /* 
+ Projeto: Conograma_capilar
+ Autor:jhonatan abreu dos santos
+ julia de almeida bareira
+Data de criação: [14/10/2025]
+ Descrição: Estrutura inicial do projeto integrador.
+*/
+
+#include <stdio.h>
+
+int main(void) {
+    printf("Iniciando o Projeto Integrador...\n");
+    return 0;
+}
